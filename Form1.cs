a2c681afcf7a5507b887ccbed3446c2ea643cd53
axelxav <axel.napitupulu@gmail.com>
2023-11-14 15:08:51 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Dish4Good_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FormRegistrasi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dish4Good_v2
+{
+    public partial class FormRegistrasi : Form
+    {
+        public FormRegistrasi()
+        {
+            InitializeComponent();
+        }
+    }
+}
