81ef0f0ddecfe4600ac20305d50988f1e5793e09
axelxav <axel.napitupulu@gmail.com>
2023-11-15 15:52:50 +0700

    Update on DonaturForm and add FormDonasiDialog as confirmation dialog for "Donatur" Role

---
+++ FormDonasiDialog.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Dish4Good_v2
+{
+    partial class FormDonasiDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tbPesanDonasi = new RichTextBox();
+            lbPesanDonasi = new Label();
+            btnCancel = new Button();
+            btnSubmit = new Button();
+            SuspendLayout();
+            // 
+            // tbPesanDonasi
+            // 
+            tbPesanDonasi.Location = new Point(151, 167);
+            tbPesanDonasi.Name = "tbPesanDonasi";
+            tbPesanDonasi.Size = new Size(493, 176);
+            tbPesanDonasi.TabIndex = 0;
+            tbPesanDonasi.Text = "";
+            // 
+            // lbPesanDonasi
+            // 
+            lbPesanDonasi.AutoSize = true;
+            lbPesanDonasi.Location = new Point(46, 167);
+            lbPesanDonasi.Name = "lbPesanDonasi";
+            lbPesanDonasi.Size = new Size(99, 20);
+            lbPesanDonasi.TabIndex = 1;
+            lbPesanDonasi.Text = "Pesan Donasi:";
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(420, 373);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(94, 29);
+            btnCancel.TabIndex = 2;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(550, 373);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(94, 29);
+            btnSubmit.TabIndex = 3;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            btnSubmit.Click += btnSubmit_Click;
+            // 
+            // FormDonasiDialog
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSubmit);
+            Controls.Add(btnCancel);
+            Controls.Add(lbPesanDonasi);
+            Controls.Add(tbPesanDonasi);
+            Name = "FormDonasiDialog";
+            Text = "FormDonasiDialog";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox tbPesanDonasi;
+        private Label lbPesanDonasi;
+        private Button btnCancel;
+        private Button btnSubmit;
+    }
+}
---
+++ FormDonasiDialog.cs
@@ -0,0 +1,83 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dish4Good_v2
+{
+    public partial class FormDonasiDialog : Form
+    {
+        private const string connectionString = "Host=localhost;Username=postgres;Password=1234;Database=Dish4Good";
+        private int requestId;
+        private int donaturId;
+
+        public FormDonasiDialog(int requestId, int donaturId)
+        {
+            InitializeComponent();
+            this.requestId = requestId;
+            this.donaturId = donaturId;
+        }
+
+        private void SimpanDonasi(int requestId, int donaturId, string pesanDonasi)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Masukkan data donasi ke tabel Donations
+                string query = "INSERT INTO Donations (request_id, donatur_id, message) VALUES (@requestId, @donaturId, @message)";
+
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@requestId", requestId);
+                    cmd.Parameters.AddWithValue("@donaturId", donaturId);
+                    cmd.Parameters.AddWithValue("@message", pesanDonasi);
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                // Update status permintaan donasi menjadi 'terpenuhi'
+                string updateStatusQuery = "UPDATE DonasiRequests SET status = 'terpenuhi' WHERE request_id = @requestId";
+
+                using (NpgsqlCommand cmd = new NpgsqlCommand(updateStatusQuery, connection))
+                {
+                    cmd.Parameters.AddWithValue("@requestId", requestId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string pesanDonasi = tbPesanDonasi.Text;
+
+            if (!string.IsNullOrEmpty(pesanDonasi))
+            {
+                // Simpan donasi ke database
+                SimpanDonasi(requestId, donaturId, pesanDonasi);
+
+                // Set DialogResult menjadi OK untuk memberi tahu form DonaturForm bahwa donasi telah dikonfirmasi
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Isi pesan donasi terlebih dahulu.", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // Set DialogResult menjadi Cancel jika pengguna membatalkan donasi
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
